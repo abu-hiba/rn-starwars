fffb4ff55292b78baf61b5b4129852b1fb1f37df
Tom Bolton <tbolton96@icloud.com>
2020-08-08 11:48:47 +0100

    Hook for retrieving character data

---
+++ hooks/swapi.js
@@ -0,0 +1,24 @@
+import { useState, useEffect } from 'react'
+
+const uri = 'https://swapi.dev/api/'
+
+const swapi = path =>
+    fetch(`${uri}${path}`).then((res) =>
+        res.status < 300 ? res.json()
+            : res.json().then((result) => Promise.reject(result))
+    )
+
+export const useCharacters = () => {
+    const [chars, setChars] = useState({ loading: true })
+
+    const fetchChars = () =>
+        swapi('people/')
+            .then(({ results }) => setChars({ results, loading: false }))
+            .catch(error => setChars({ error, loading: false }))
+
+    useEffect(() => {
+        fetchChars()
+    }, [])    
+
+    return { chars }
+}
